251636d5ef5c343cf307ea2cb4799f20207f646f
baiyunfei <1302853768@qq.com>
2024-09-10 20:01:01 +0800

    what is the answer

--- code.java
+++ code.java
@@ -3,15 +3,15 @@ public class code {
     private int age;
     private String name;
     private int sex;
-    private int third;
+    private int myfinal;
 
 
     public int getDds() {
-        return timesss;
+        return myfinal;
     }
 
     public void setDd(int dd) {
-        this.timesss = dd;
+        this.myfinal = dd;
     }
 
     public int getAgesssd() {
