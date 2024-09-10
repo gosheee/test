6e1af76744d2aff4ff18315064996909863b17c8
baiyunfei <1302853768@qq.com>
2024-09-10 17:22:41 +0800

    this is a test

---
+++ code.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+public class code {
+    private int age;
+    private String name;
+    private int sex;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
